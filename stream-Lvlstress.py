4ef5c867e6c1dc9674cd22c39aa74421fda5f37d
Khaevahasna26 <120515877+Khaevahasna26@users.noreply.github.com>
2022-12-14 12:50:03 +0700

    Add files via upload

---
+++ stream-Lvlstress.py
@@ -0,0 +1,38 @@
+import pickle
+import streamlit as st
+ 
+# load save model
+model = pickle.load(open('Lvstress_Model.sav', 'rb'))
+
+# Judul Untuk Web
+st.title('Data Mining Prediksi Tingkat level Stress')
+
+st.subheader('Nama : Khaeva Hasna') 
+st.subheader('Nim : 191351047')
+
+ 
+# Form Input
+Humidity = st.text_input('Masukan Nilai Humidity')
+
+Temperature = st.text_input('Masukan Nilai Temperature') 
+
+Step_count = st.text_input('Masukan  Nilai Step count')
+
+# kode Prediksi
+
+stress_diagnosis = ' '
+
+#Button Prediksi
+if st.button('Test'):
+    stress_prediction = model.predict([[Humidity, Temperature, Step_count]])
+
+    if(stress_prediction[0]==0):
+        stress_diagnosis = 'Tingkat Stress Rendah'
+
+    elif(stress_prediction[0]==1):
+          stress_diagnosis = 'Tingkat Stress Sedang'
+
+    else:
+         stress_diagnosis = 'Tingkat Stress Tinggi'
+
+st.success(stress_diagnosis) 
